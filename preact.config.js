31a056bf7c449140e990d2b6bcc37f8654880064
Navbryce <plunkett.bryce.m@gmail.com>
2022-05-15 20:58:43 -0400

    Update postcss processing

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,6 @@
 import {DefinePlugin} from 'webpack';
 
-const { required } = require('dotenv-safe').config();
+const {required} = require('dotenv-safe').config();
 
 export default (config, env, helpers, params = defaultParams) => {
     config.resolve.alias.src = env.src;
@@ -14,31 +14,4 @@ export default (config, env, helpers, params = defaultParams) => {
             {}
         ))
     );
-
-    // tailwind
-    const postCssLoaders = helpers.getLoadersByName(config, 'postcss-loader');
-    postCssLoaders.forEach(({ loader }) => {
-        const plugins = loader.options.postcssOptions.plugins;
-
-        // Add tailwind css at the top.
-        plugins.unshift(require('tailwindcss'));
-
-        // for purging css to make build smaller
-        const purgecss = require('@fullhuman/postcss-purgecss')({
-            // Specify the paths to all of the template files in your project
-            content: [
-                './src/**/*.js',
-                './src/**/*.jsx',
-                './src/**/*.ts',
-                './src/**/*.tsx',
-            ],
-            safelist: {greedy: [/swal2/]}, // safelist for sweetalert2 dialog styles
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        });
-        if (process.env.NETLIFY) {
-            // minimize build for deploys
-            plugins.push(purgecss);
-        }
-    });
-    return config;
 }
